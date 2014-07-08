aa6b48766cd4397e3d8979767100d91823c894a5
Flow86 <github@fd.mytrap.de>
2014-07-08 22:43:41 +0200

    add ic2 api etc

--- src/main/java/missingrecipes/missingrecipes.java
+++ src/main/java/missingrecipes/missingrecipes.java
@@ -2,24 +2,25 @@ package missingrecipes;
 
 import java.util.ArrayList;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraftforge.oredict.OreDictionary;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(name = "Missing-Recipes", version = missingrecipes.VERSION, useMetadata = false, modid = "Missing-Recipes", dependencies = "required-after:Forge@[7.7.2.682,);after:Forestry")
+@Mod(name = "Missing-Recipes", version = missingrecipes.VERSION, useMetadata = false, modid = "Missing-Recipes", dependencies = "required-after:Forge@[7.7.2.682,);after:Forestry;after:IC2")
 public class missingrecipes {
 	public static final String VERSION = "@MR_VERSION@";
 
 	@Instance("Missing-Recipes")
 	public static missingrecipes instance;
 
-	@Init
+	@Mod.EventHandler
 	public void initialize(FMLInitializationEvent evt) {
 
+		// forestry
 		ArrayList<ItemStack> apatiteOres = OreDictionary.getOres("oreApatite");
 		ArrayList<ItemStack> apatiteGems = OreDictionary.getOres("gemApatite");
 
@@ -29,7 +30,12 @@ public class missingrecipes {
 
 			apatiteGem.stackSize = 4;
 
-			FurnaceRecipes.smelting().addSmelting(apatiteOre.getItem().itemID, apatiteOre.getItemDamage(), apatiteGem, 0.5f);
+			GameRegistry.addSmelting(Block.getBlockFromItem(apatiteOre.getItem()), apatiteGem, 0.5f);
 		}
+
+		// diamonds
+		// ic2
+		// GameRegistry.addSmelting(Block.getBlockFromItem(apatiteOre.getItem()), apatiteGem, 0.5f);
+
 	}
 }
